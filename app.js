b7bbff7d54900233b8947d77ffc2f2f1ea5cd3ab
YeonwooSung <neos960518@gmail.com>
2018-07-02 15:06:29 +0900

    add mysql library

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var aws = require('aws-sdk');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
