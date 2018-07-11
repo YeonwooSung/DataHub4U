a0dd163d55a2de4ad423861553802fe1870afe15
YeonwooSung <neos960518@gmail.com>
2018-07-12 00:27:05 +0900

    add the router for the '/data'

---
+++ routes/data.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/data', function(req, res, next) {
+    //TODO complete this method.
+});
+
+module.exports = router;
