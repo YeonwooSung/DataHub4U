73fa73042327514a0533074e9f0c2301f6849ecc
YeonwooSung <neos960518@gmail.com>
2018-07-12 01:32:40 +0900

    make a new file for the https connection

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-const PORT = 80;
+const PORT = 443;
 
 var createError = require('http-errors');
 var express = require('express');
@@ -29,23 +29,23 @@ app.use('/data', dataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 //set the port number that the server uses.
 app.listen(PORT, function() {
-  console.log("listen to the port number: " + PORT);
+    console.log("listen to the port number: " + PORT);
 });
 
 
