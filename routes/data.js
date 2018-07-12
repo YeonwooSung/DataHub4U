ece755df41b2f0bad5cbf53e36b7e268b6bbfdcd
YeonwooSung <neos960518@gmail.com>
2018-07-12 19:03:34 +0900

    make the jade file for the '/data' url

--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET data page */
 router.get('/data', function(req, res, next) {
-    //TODO complete this method.
+    res.render('data', { title: 'Data Thing' });
 });
 
 module.exports = router;
