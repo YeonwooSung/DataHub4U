c4c416c2e42f850caad981fb49979acf1f6ffa86
YeonwooSung <neos960518@gmail.com>
2018-07-13 00:05:37 +0900

    documentation

--- routes/collect.js
+++ routes/collect.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../api/sqlConnection');
+
 /* GET */
 router.get('/', function(req, res) {
     var temperature = req.query.temperature;
