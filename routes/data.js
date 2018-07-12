cb8679cd11c746848b76073fb1fffbef0c431420
YeonwooSung <neos960518@gmail.com>
2018-07-12 23:14:19 +0900

    Remove the debugging stuffs.

--- routes/data.js
+++ routes/data.js
@@ -3,13 +3,11 @@ var router = express.Router();
 
 /* GET data page */
 router.get('/', function(req, res) {
-    console.log("hi");
     res.render('data', { title: 'Data Thing' });
 });
 
 /* GET data analysis page */
 router.get('/analysis', function (req, res) {
-    console.log("hi!!");
     res.render('data', { title: 'Analysis' });
 });
 
