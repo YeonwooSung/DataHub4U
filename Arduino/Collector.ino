d1a60073963c9c52003c8f980c48e3e78d50327b
YeonwooSung <neos960518@gmail.com>
2018-07-12 18:18:05 +0900

    change the port number

--- Arduino/Collector.ino
+++ Arduino/Collector.ino
@@ -27,7 +27,7 @@ const char *ssid = "";
 const char *password = "";
 
 const int firstIndex = 0;
-const httpPort = 80; //the port number for the http connection (use the default http port number)
+const httpPort = 8080; //the port number for the http connection (use the default http port number)
 
 const char* host = ""; //the name of the host
 String url = ""; //the target URL
