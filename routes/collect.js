a669bfd7f03db23378e13d5422518fba5067f14b
YeonwooSung <neos960518@gmail.com>
2018-07-14 02:19:23 +0900

    removed unnecessary debugging messages

--- routes/collect.js
+++ routes/collect.js
@@ -9,10 +9,6 @@ router.get('/', function(req, res) {
     var latitude = req.query.latitude;
     var longitude = req.query.longitude;
 
-    console.log("\ntemperature: " + temperature);
-    console.log("latitude: " + latitude);
-    console.log("longitude: " + longitude);
-
     db.insertIntoTable("table", temperature, latitude, longitude); //TODO use the suitable sql table name!!
 
     res.render('collect', { title: 'Temperature data'});
