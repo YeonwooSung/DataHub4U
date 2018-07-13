cfc522b40021a9d1b26fa93a3ac9b3318b21cbba
YeonwooSung <neos960518@gmail.com>
2018-07-13 21:54:38 +0900

    connect the collect router and sqlConnection file.

--- routes/collect.js
+++ routes/collect.js
@@ -9,9 +9,11 @@ router.get('/', function(req, res) {
     var latitude = req.query.latitude;
     var longitude = req.query.longitude;
 
-    console.log(temperature);
-    console.log(latitude);
-    console.log(longitude);
+    console.log("temperature: " + temperature);
+    console.log("latitude: " + latitude);
+    console.log("longitude: " + longitude + "\n");
+
+    db.insertIntoTable("table", temperature, latitude, longitude); //TODO use the suitable sql table name!!
 
     res.render('collect', { title: 'Temperature data'});
 });
