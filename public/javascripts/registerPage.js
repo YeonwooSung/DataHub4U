d40206ea6e60786b6391e6a8fda72976664e6b19
YeonwooSung <neos960518@gmail.com>
2018-09-08 01:04:42 +0900

    fix the error in the register page

---
+++ public/javascripts/registerPage.js
@@ -0,0 +1,49 @@
+/**
+ * This function checks if the values of the attributes and confirm-attributes are same.
+ *
+ * @return {boolean}
+ */
+function confirmUserInfo() {
+    let element = document.getElementById('hidden_div').childNodes[0];
+
+    if (element) {
+        document.getElementById('hidden_div').removeChild(element);
+    }
+
+    let password = document.getElementById('password').value;
+    let confirmedPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmedPassword) {
+
+        makeNewElement('Please recheck the password and confirm-password.', 'passwordDiv');
+        return false;
+
+    }
+
+    let email = document.getElementById('email').value;
+    let confirmedEmail = document.getElementById('confirm-email').value;
+
+    if (email !== confirmedEmail) {
+        makeNewElement('Please recheck the email and confirm-email sections.', 'emailDiv');
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * This function makes the new div tag to display the warning message, and appends it to the register page.
+ *
+ * @param text
+ */
+function makeNewElement(text) {
+    if (document.getElementById(id)) {
+        return;
+    }
+    let newDiv = document.createElement('div');
+    newDiv.innerText = text;
+    newDiv.style.color = 'red';
+    newDiv.style.fontSize = '3vmin';
+
+    document.getElementById('hidden_div').appendChild(newDiv);
+}
